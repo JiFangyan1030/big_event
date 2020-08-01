b4ebd8a3214031d84c83b97e933fea1c0b4c96d4
jifangyan <jfy19961030@163.com>
2020-08-01 14:37:06 +0800

    index页面完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,30 @@
 var baseURL = 'http://ajax.frontend.itheima.net'
 // 拦截/过滤每一次的ajax请求.配置每次请求需要的参数
 $.ajaxPrefilter(function (options) {
-    console.log(options)
-    console.log(options.url)
+    // console.log(options)
+    // console.log(options.url)
+    // 在发起整整的Ajax请求之前同意拼接请求的路径
     options.url = baseURL + options.url
+    // 统一为有权限的接口,设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    // complete函数就是无论成功或是是否都会调用这个函数
+
+    options.complete = function (res) {
+        console.log(res)
+        // console.log(res.responseJSON.status)
+        // console.log(res.responseJSON.message)
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 强制清空token
+            localStorage.removeItem('token');
+            // 强制跳转到登录页面
+            location.href = '/login.html';
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,72 @@
+$(function () {
+    // 获取用户信息
+    getUserInfo();
+
+    var layer = layui.layer;
+    // 点击实现退出功能
+    $('#btnLogout').on('click', function () {
+        // layui的弹出提示框
+        layer.confirm('是否确认退出', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            // 先关闭提示框
+            console.log(111);
+            layer.close(index);
+            // 然后删除本地的token
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: '/my/userinfo',
+        //jQuery中的ajax headers专门用于设置请求头信息的原生js中是Request Headers
+        // headers属性区分大小写
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ""
+        // },
+        // 在baseAPI中统一使用complete函数,complete函数无论执行成功或者失败都会执行
+        // success: function (res) {
+        //     // token可能24小时就失效;所以需要重新登录
+        //     // console.log(res)
+        //     if (res.status !== 0) {
+        //         return layui.layer.msg('获取用户信息失败')
+        //     }
+        //     // 调用renderAvatar渲染用户头像
+        //     console.log(res)
+        //     renderAvatar(res.data)
+        // }
+        // complete: function (res) {
+        //     console.log(res)
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+        //         // 强制清空token
+        //         localStorage.removeItem('token');
+        //         // 强制跳转到登录页面
+        //         location.href = '/login.html';
+        //     }
+        // }
+
+    })
+}
+// 用户渲染函数
+function renderAvatar(user) {
+    // 1.渲染用户名
+    var uname = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + uname)
+    console.log(uname[0])
+    // 渲染用户头像
+    // 判断,用户头像信息.有就渲染图片.没有就渲染文字
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').show().html(uname[0].toUpperCase());
+    }
+
+}
